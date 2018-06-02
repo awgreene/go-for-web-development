ccaa2b69a3d0409bd2f9eaa3ec00ee92be391096
agreene <agreene@redhat.com>
2018-06-02 12:13:39 -0400

    Complete section 3.1

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/yosssi/ace"
 
 	"encoding/json"
 	"encoding/xml"
@@ -39,20 +39,22 @@ func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFun
 }
 
 func main() {
-	templates := template.Must(template.ParseFiles("templates/index.html"))
-
 	db, _ = sql.Open("sqlite3", "dev.db")
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template, err := ace.Load("templates/index", "", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
 		p.DBStatus = db.Ping() == nil
 
-		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+		if err := template.Execute(w, p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
